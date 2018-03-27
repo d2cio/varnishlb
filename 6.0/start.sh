7405b98a474c6d94c15e195fe397a2081b6959ea
Pavel <picnixer@gmail.com>
2018-03-27 14:51:44 +0500

    fix start command

--- 6.0/start.sh
+++ 6.0/start.sh
@@ -3,6 +3,7 @@
 set -e
 
 exec bash -c \
-  "exec varnishd -a 0.0.0.0:$PORT,PROXY,user=vcache,group=varnish,mode=660 \
-  -f /etc/varnish/default.vcl -F \
-  -s malloc,$CACHE_SIZE"
+  "exec varnishd -j unix,user=vcache -F \
+  -f /etc/varnish/default.vcl \
+  -s malloc,$CACHE_SIZE \
+  -a 0.0.0.0:$PORT"
